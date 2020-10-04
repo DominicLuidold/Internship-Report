f760df5551b965206163a81dbe83ab2a7d1e435e
Dominic Luidold <dominic.luidold@gmail.com>
2020-10-04 21:46:49 +0200

    Work on "Umsetzung" and chapter 3.1

---
+++ code/region_controller_v1.php
@@ -0,0 +1,30 @@
+<?php
+
+[...]
+
+final class RegionController
+{
+    [...]
+
+    /**
+     * @Route("/regions", name="get_regions", methods={"GET"})
+     */
+    public function cgetAction(Request $request): Response {
+        // Authentication
+        // Several function calls of "RegionManager"
+        // Preperation of Data Transfer Objects
+        // Returning response with DTOs
+    }
+
+    /**
+     * @Route("/regions/{id}", name="get_region", methods={"GET"})
+     */
+    public function getAction(int $id): Response { // Implementation }
+
+    /**
+     * @Route("/regions/{id}", name="patch_region", methods={"PATCH"})
+     */
+    public function patchAction(int $id, Request $request): Response { // Implementation }
+
+    [...]
+}
