f64d54afb2bc1bd98d2a676e3a6e2ff9aeaa2414
Dominic Luidold <dominic.luidold@gmail.com>
2020-10-13 17:32:36 +0200

    Fix minor text imperfections

--- code/project_controller_test.php
+++ code/project_controller_test.php
@@ -4,13 +4,17 @@
 
 class ProjectControllerTest
 {
-    // Set up for tests
-    
     public function testGetProjects(): void
     {
         $this->makeJsonRequest('GET', '/api/projects');
-        $response = \json_decode(self::$client->getResponse()->getContent(), true);
-        self::assertSame(Response::HTTP_OK, self::$client->getResponse()->getStatusCode());
+        $response = \json_decode(
+            self::$client->getResponse()->getContent(),
+            true
+        );
+        self::assertSame(
+            Response::HTTP_OK,
+            self::$client->getResponse()->getStatusCode()
+        );
         
         self::assertCount(1, $response['_embedded'];
         // Other specific response validation
--- code/region_controller_v1.php
+++ code/region_controller_v1.php
@@ -4,12 +4,11 @@
 
 final class RegionController
 {
-    [...]
-
     /**
      * @Route("/regions", name="get_regions", methods={"GET"})
      */
-    public function cgetAction(Request $request): Response {
+    public function cgetAction(Request $request): Response
+    {
         // Authentication
         // Several function calls of "RegionManager"
         // Preperation of Data Transfer Objects
@@ -19,12 +18,12 @@ final class RegionController
     /**
      * @Route("/regions/{id}", name="get_region", methods={"GET"})
      */
-    public function getAction(int $id): Response { // Implementation }
+    public function getAction(int $id): Response { /* Implementation */ }
 
     /**
      * @Route("/regions/{id}", name="patch_region", methods={"PATCH"})
      */
-    public function patchAction(int $id, Request $request): Response { // Implementation }
+    public function patchAction(int $id, Request $request): Response { /* Implementation */ }
 
     [...]
 }
